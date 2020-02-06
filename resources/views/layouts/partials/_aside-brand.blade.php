5847f8d0c1abb2ec4d394bc7ce9aa1066fefdbf4
UnivDev <univdev99@gmail.com>
2020-02-06 09:06:28 -0500

    Fix bug

--- resources/views/layouts/partials/_aside-brand.blade.php
+++ resources/views/layouts/partials/_aside-brand.blade.php
@@ -2,7 +2,7 @@
 <div class="kt-aside__brand kt-grid__item " id="kt_aside_brand">
     <div class="kt-aside__brand-logo">
         <a href="">
-            <img alt="Logo" src="/assets/media/logos/logo-light.png"/>
+            <img alt="Logo" src="/assets/media/logos/GoHundred-icon.png"/>
         </a>
     </div>
     <div class="kt-aside__brand-tools">
