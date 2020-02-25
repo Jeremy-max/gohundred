c28efae9b9ea66be823d85534566b27649bafe54
UnivDev <univdev99@gmail.com>
2020-02-25 18:07:33 -0500

    Fix migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,18 +20,15 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('country');
             $table->string('password');
-
             $table->boolean('active')->default(true);
             $table->boolean('login_via_facebook')->default(false);
             $table->boolean('login_via_google')->default(false);
             $table->timestamp('payment_status')->nullable();
             $table->text('comment')->nullable();
-
             $table->string('stripe_id')->nullable()->collation('utf8mb4_bin');
             $table->string('card_brand')->nullable();
             $table->string('card_last_four', 4)->nullable();
             $table->timestamp('trial_ends_at')->nullable();
-
             $table->rememberToken();
             $table->timestamps();
         });
